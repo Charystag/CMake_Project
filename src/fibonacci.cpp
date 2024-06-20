7dd5e0113467370bcaf28c9baa06c824705f6f94
Charystag <57284562+Charystag@users.noreply.github.com>
2024-06-20 16:29:37 +0200

    feat(fibonacci): Added fibonacci implementation

---
+++ src/fibonacci.cpp
@@ -0,0 +1,20 @@
+#include "fibonacci.h"
+
+/*
+Make some researches about memoization (not memorization) to 
+understand the solution to this exercise
+*/
+int	fibonacci(int a){
+	static int	terms[100];
+
+	if (a < 0)
+		return (0);
+	if (a < 3)
+		return (1);
+	if (a > 99)
+		return (0);
+	if (terms[a] != 0)
+		return (terms[a]);
+	terms[a] = fibonacci(a - 1) + fibonacci(a - 2);
+	return (terms[a]);
+}
