5d84f3ac0b95964def7848159e1afcc3b1688367
Charystag <57284562+Charystag@users.noreply.github.com>
2024-06-20 16:29:59 +0200

    feat(palindrome): Added palindrome implementation

---
+++ include/palindrome.h
@@ -0,0 +1,6 @@
+#ifndef __PALINDROME_H__
+# define __PALINDROME_H__
+
+int	palindrome(const char *);
+
+#endif
---
+++ src/palindrome.cpp
@@ -0,0 +1,20 @@
+#include "palindrome.h"
+#include <cstring>
+
+int	palindrome(const char * s){
+	size_t	len;
+	size_t	end;
+
+	len = std::strlen(s);
+	if (! len)
+		return (0);
+	beg = 0;
+	--len;
+	while (beg <= len){
+		if (*(s + beg) != *(s + len))
+			return (0);
+		--len;
+		++beg;
+	}
+	return (1);
+}
