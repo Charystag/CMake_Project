d309b2850b5e8bf0954f6921a59e0f59737fc8d6
Charystag <57284562+Charystag@users.noreply.github.com>
2024-06-21 09:47:19 +0200

    Fixed type size_t -> int for palindrome

--- src/palindrome.cpp
+++ src/palindrome.cpp
@@ -1,13 +1,15 @@
 #include "palindrome.h"
 #include <cstring>
+#include <iostream>
 
 int	palindrome(const char * s){
-	size_t	len;
-	size_t	beg;
+	int	len;
+	int	beg;
 
 	len = std::strlen(s);
 	if (! len)
 		return (0);
+	std::cout << "this is the length : " << len << std::endl;
 	beg = 0;
 	--len;
 	while (beg <= len){
