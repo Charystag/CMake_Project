848bfb71f5f86507e20c7258b5bd8cd33ce3ee80
Charystag <57284562+Charystag@users.noreply.github.com>
2024-06-21 11:02:13 +0200

    docs: Fixed documentation and moved some in example_class.cpp

--- src/example_class.cpp
+++ src/example_class.cpp
@@ -1,11 +1,27 @@
 #include "example_class.h"
+/**
+ *@file
+ *@brief implementation of example class
+ */
 
+/**
+ *@brief constructor
+ *@description sets the value val to 0
+ */
 example_class::example_class() : val(0) {}
 
+/**
+ *@brief getter for the value val
+ *@return the value of the variable val
+ */
 void	example_class::setVal(int value){
 	val = value;
 }
 
+/**
+ *@brief setter for the value val
+ *@param the new value to set for the variable val
+ */
 int	example_class::getVal(){
 	return (val);
 }
